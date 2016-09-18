0498b4dd0e3fa1f1cfce8de3c5cbf291925ba1f6
Pietro909 <pietro.grandi.3d@gmail.com>
2016-09-18 11:27:49 +0200

    add ngrx/store

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Store, StoreModule} from "@ngrx/store";
+import { ActionReducer, Action } from '@ngrx/store';
 
 import { AppComponent }  from './app.component';
 
+export const myReducer: ActionReducer<CurrentSearch> = (state: CurrentSearch, action: Action) => {
+return state;
+}
+
+const storeManager = StoreModule.provideStore ({ currentSearch: myReducer });
+
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, StoreModule, storeModule],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
